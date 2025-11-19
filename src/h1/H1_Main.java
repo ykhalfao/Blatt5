d342e77e3e3c324e851f8237cd2c1022ec817e54
ykhalfao <ykhalfao@smail.uni-koeln.de>
2025-11-19 19:55:04 +0100

    Blatt vollstaendig geloest und ueberarbeitet 1.0

--- src/h1/H1_Main.java
+++ src/h1/H1_Main.java
@@ -2,22 +2,14 @@ package h1;
 
 public class H1_Main {
     public static void main(String[] args) {
-        int a=4;
-        int b=4;
-        if (b==a) {
+        int[] myArray = {5, 6, 7, 10};
+        int[] myArray2 = new int[myArray.length];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray2[i] = myArray[myArray.length - i - 1];
+            }
 
-            int [] myArray = {5,6,7,10}; //eigentlich: int [] myArray;
-                                                    // myArray = new int [a]; (wenn das Array laenger waere muesste man das Programm entsprechend verlaengern)
-            int[] myArray2;
-            myArray2 = new int [b];
-            myArray2 [0] = myArray [a-(a-3)];   //ist wie myArray [0] = myArray [3]
-            myArray2 [a-(a-1)] = myArray [a-(a-2)];   //untere Zeilen Analog
-            myArray2 [a-(a-2)] = myArray [a-(a-1)];
-            myArray2 [a-(a-3)] = myArray [0];
-            myArray = myArray2;
-            System.out.println(myArray [a-1]);
-        }
-
-
+        for (int l = 0; l < myArray.length; l++) {
+            System.out.print(myArray2[l] + " ");
+    }
     }
 }
--- src/h2/H2_Main.java
+++ src/h2/H2_Main.java
@@ -8,24 +8,24 @@ public class H2_Main {
         int[] a = new int[9];       //9 Zellen voller Nullen
 
         if (n == 0) {
-            digits =1;              //wenn n = 0 funktioniert die untere Schleife nicht deswegen digits direkt = 1
-        }else {
-            int nDigits =n;
-            while (nDigits>0) {
-                nDigits =nDigits/10;
+            digits = 1;              //wenn n = 0 funktioniert die untere Schleife nicht deswegen digits direkt = 1
+        } else {
+            int nDigits = n;
+            while (nDigits > 0) {
+                nDigits = nDigits / 10;
                 digits++;           //digits wird = Anzahl: ziffern & n bleibt gleich
             }
         }
-        int aHinten =8;     //Array von hinten fuellen
-        int nA= n;          //n veraendert sich hierdurch nicht
-        while (nA>0) {      //Array mit n fuellen
-            a[aHinten] = nA%10;
-            nA= nA/10;
+        int aHinten = 8;     //Array von hinten fuellen
+        int nA = n;          //n veraendert sich hierdurch nicht
+        while (nA > 0) {      //Array mit n fuellen
+            a[aHinten] = nA % 10;
+            nA = nA / 10;
             aHinten--;
         } // ______Test Ausgabe______//
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
-        System.out.println("n Zifferanzahl: "+digits);
+        System.out.println("n Zifferanzahl: " + digits);
     }
 }
